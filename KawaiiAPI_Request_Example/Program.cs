0db03d93c3d31d58b4eb6dd3d01b4bb79be72f72
LedgendODST <huettich97@gmx.de>
2021-09-07 20:48:23 +0200

    kawaii.red API Request Example Create

---
+++ KawaiiAPI_Request_Example/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace KawaiiAPI_Request_Example
+{
+    public static class Program
+    {
+        public static IHostBuilder CreateHostingConfig(string[] args)
+        {
+            //Adds all needed Services to the collection.
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, collection) => collection
+                    .AddHttpClient()
+                    .AddSingleton<PrintApiDetails>()
+                    .AddHostedService(x => x.GetRequiredService<PrintApiDetails>()));
+
+            return hostBuilder;
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using var host = CreateHostingConfig(args).Build();
+                host.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+    }
+}
